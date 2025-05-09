784edab1e85042aa90cfd4aacee27c6480bb9f1d
Nathandiv <k.n.hlungwani@gmail.com>
2025-05-09 16:00:45 +0200

    Implement main banking services menu in Main.java

--- Main.java
+++ Main.java
@@ -2,6 +2,20 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        float balance = 5000.0f;
+
+        System.out.println("WELCOME TO GL Banking Services");
+        System.out.println("Which service would you like to do?");
+        System.out.println("1. Deposit");
+        System.out.println("2. Check balance");
+        System.out.println("3. Transfer");
+        System.out.print("Enter 1, 2, or 3: ");
+        int choice = input.nextInt();
+
+       
+        input.close(); 
        
     }
 }
